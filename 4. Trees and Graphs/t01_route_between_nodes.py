47ee8c87406f02d11f4acd570b655ebaceae3479
Luca Cesarano <lu.cesarano@reply.it>
2022-09-10 16:19:02 +0200

    Added exercise 4.1 and data structures

---
+++ 4. Trees and Graphs/t01_route_between_nodes.py
@@ -0,0 +1,83 @@
+import unittest
+
+
+def is_route_dfs(graph, start, end, visited=None):
+    if start == end:
+        return True
+
+    if visited is None:
+        visited = set()
+
+    for node in graph[start]:
+        if node not in visited:
+            visited.add(node)
+            if node == end or is_route_dfs(graph, node, end, visited):
+                return True
+
+    return False
+
+
+def is_route_bfs(graph, start, end):
+    if start == end:
+        return True
+
+    visited = set()
+    queue = [start]
+
+    while queue:
+        node = queue.pop()
+        for adj in graph[node]:
+            if adj not in visited:
+                if adj == end:
+                    return True
+                else:
+                    queue.append(adj)
+        visited.add(node)
+    return False
+
+
+class Test(unittest.TestCase):
+    graph = {
+        "A": ["B", "C"],
+        "B": ["D"],
+        "C": ["D", "E"],
+        "D": ["B", "C"],
+        "E": ["C", "F"],
+        "F": ["E", "O", "I", "G"],
+        "G": ["F", "H"],
+        "H": ["G"],
+        "I": ["F", "J"],
+        "O": ["F"],
+        "J": ["K", "L", "I"],
+        "K": ["J"],
+        "L": ["J"],
+        "P": ["Q", "R"],
+        "Q": ["P", "R"],
+        "R": ["P", "Q"],
+    }
+
+    tests = [
+        ("A", "L", True),
+        ("A", "B", True),
+        ("H", "K", True),
+        ("L", "D", True),
+        ("P", "Q", True),
+        ("Q", "P", True),
+        ("Q", "G", False),
+        ("R", "A", False),
+        ("P", "B", False),
+    ]
+
+    def test_is_route(self):
+        for [start, end, expected] in self.tests:
+            actual = is_route_dfs(self.graph, start, end)
+            assert actual == expected
+
+    def test_is_route_bfs(self):
+        for [start, end, expected] in self.tests:
+            actual = is_route_bfs(self.graph, start, end)
+            assert actual == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
